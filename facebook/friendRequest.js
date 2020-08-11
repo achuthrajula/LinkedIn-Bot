c1001706a450cfb907195ef01f2719be11276793
achuthrajula <achuthrajula1999@gmail.com>
2020-08-11 16:35:23 +0530

    Facebook Groups

--- facebook/friendRequest.js
+++ facebook/friendRequest.js
@@ -2,9 +2,7 @@ const {Builder, By, Key, until, WebDriver} = require('selenium-webdriver');
 const { del } = require('selenium-webdriver/http');
 
 (async function example() {
-  WebDriver.createSession('a94f8ff3a42faf2b5520d329c146a59c')
   let driver = await new Builder().forBrowser('chrome').build();
-  console.log(driver.getSession())
     let users = [
     //   'https://m.facebook.com/achuth09',
       'https://www.facebook.com/gouthamgandhi'
@@ -28,15 +26,7 @@ const { del } = require('selenium-webdriver/http');
 
       // message button                     
       await driver.findElement(By.className('FriendRequestAdd')).click()
-      delay()
-    //   // select and type message
-    //   await driver.navigate().refresh()
-    //   delay()
-    //   await driver.findElement(By.css('textarea[placeholder="Write a message..."]')).sendKeys('Hi, please ignore this message it\'s sent to test out features in real time', Key.RETURN)
-    //   delay()
-    //   // send
-    //   await driver.findElement(By.xpath('//button[@value="Send"]')).sendKeys(Key.RETURN)
-    //   delay()
+      await delay()
     }
     async function processArray(array) {
       for (const item of array) {
@@ -47,4 +37,3 @@ const { del } = require('selenium-webdriver/http');
     processArray(users)
   }
 })();
-
---
+++ facebook/groups.js
@@ -0,0 +1,96 @@
+const {Builder, By, Key, until, WebDriver} = require('selenium-webdriver');
+const fs = require('fs')
+const { del } = require('selenium-webdriver/http');
+
+// (async function example() {
+//     let driver = await new Builder().forBrowser('chrome').build(); 
+
+// try {
+//     await driver.get('https://www.youtube.com/');
+    
+// }
+
+// finally {
+    
+//     for(let i = 0; i < 100000; i+=1000) {
+//         await driver.executeScript(`scroll(0,${i})`);
+//         await new Promise(r => setTimeout(r, 2000));
+//         console.log(i)
+//     }
+// }
+// })();
+
+(async function example() {
+  let driver = await new Builder().forBrowser('chrome').build();
+    let groupUrls = []
+    let groups = Array.from(Array(1).keys())
+
+    function storeURLs(url) {
+        groupUrls.push(url)
+    }
+
+    function delay() {
+        let delay = Math.floor(Math.random() * (5000 - 2000)) + 2000
+        console.log('delay', delay)
+        return new Promise(resolve => setTimeout(resolve, delay))
+    }
+
+  try {
+    await driver.get('https://en-gb.facebook.com/');
+    await delay()
+    await driver.findElement(By.name('email')).sendKeys('achuth.rajula@ftlabs.io');
+    await delay()
+    await driver.findElement(By.name('pass')).sendKeys('7981936393');
+    await delay()
+    await driver.findElement(By.id('loginbutton')).click();
+    await delay()
+    await driver.get('https://m.facebook.com/groups_browse/your_groups/');
+    await delay()
+  } finally {
+    // await driver.quit();
+    async function post (url) {
+        await delay();
+        await driver.get(url);
+        await delay();
+        await driver.findElement(By.xpath('//div[text()="Write something..."]')).click();
+        await delay();
+        await driver.findElement(By.css('textarea[aria-label="What\'s on your mind?"]')).sendKeys('Hi this is riku, for more details visit my website https://www.getriku.com/');
+        await delay();
+        await driver.findElements(By.css('button[type="submit"][value="Post"]'))
+        .then((e) => {
+            for(var key in e){
+                    var element = e[key];
+                    if(element.isDisplayed()){
+                    element.click();            
+                }
+            }
+        })
+        .catch((e) => console.log(e));
+        await delay();
+    }
+    async function delayedLog(item) {
+        for(let i = 0; i < 10000; i+=1000) {
+            await driver.executeScript(`scroll(0,${i})`);
+            await new Promise(random => setTimeout(random, 1000));
+            console.log(i);
+        }
+        await delay();
+        await driver.findElements(By.css('a[href*="?ref=group_browse"]'))
+        .then((e) => e.map((sub) => sub.getAttribute('href').then((val) => storeURLs(val))))
+        .catch((e) => console.log(e))
+        await delay();
+        for(const url of groupUrls) {
+            await post(url);
+        }
+        await delay();
+    }
+    async function processArray(array) {
+        for (const item of array) {
+            await delayedLog(item);
+        }
+        console.log('Done!');
+    }
+    processArray(groups);
+  }
+})();
+
